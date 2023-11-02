2a857b84650b7fea70e5fc6a058c1daf20b4710f
imyakin <myakinalex@yandex.ru>
2023-11-02 12:40:11 +0400

    add sass, postcss, browserlist, hotreloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,46 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-let mode = 'development';
+let mode = "development";
 
-if(process.env.NODE_ENV){
-    mode = 'production';
+if (process.env.NODE_ENV) {
+  mode = "production";
 }
 
 module.exports = {
-    mode: mode,
+  mode: mode,
 
-    plugins: [new MiniCssExtractPlugin()],
+  plugins: [new MiniCssExtractPlugin()],
 
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader']
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            }
-        ]
-    },
-
-    devServer: {
-        static: {
-            directory: path.join(__dirname, "dist")
+  module: {
+    rules: [
+      {
+        test: /\.(s[ac]|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
         },
-        compress: true,
-        port: 8080,
-    },
+      },
+    ],
+  },
 
-    devtool: 'source-map'
-}
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 8080,
+    hot: true,
+  },
+
+  devtool: "source-map",
+};
